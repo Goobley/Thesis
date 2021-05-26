50a4759a4947070d68a7bbab555b8b8e53840428
cmo <c.osborne.1@research.gla.ac.uk>
2021-05-26 18:38:16 +0100

    Set up gitignore

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source ~/.bashrc
+CONDA="${CMO_CONDA_PATH}/bin/conda"
+CONDA_BASE=$(${CONDA} info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
+conda activate LightweaverDev
+
+latexmk -r pythontex-latexmkrc -pdf --shell-escape -synctex=1 -interaction=nonstopmode -file-line-error $1
