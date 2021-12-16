88487f5baa67dd29425355ece7ed90c0433d5233
cmo <c.osborne.1@research.gla.ac.uk>
2021-12-16 20:58:20 +0000

    Updated build script and environment for clean build

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ source ~/.bashrc
 CONDA="${CMO_CONDA_PATH}/bin/conda"
 CONDA_BASE=$(${CONDA} info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh
-conda activate LightweaverDev
+conda activate Thesis
 
 latexmk -r pythontex-latexmkrc -pdf --shell-escape -synctex=1 -interaction=nonstopmode -file-line-error $1
